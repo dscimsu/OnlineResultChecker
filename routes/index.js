686e0fbf36e351ae3544773b59a9f565a479356f
rexsteroxy <rexsteroxy22@gmail.com>
2020-04-10 10:26:57 +0100

    restructured the routes and added comments

--- routes/index.js
+++ routes/index.js
@@ -1,60 +1,27 @@
 let express = require("express");
 let router = express.Router();
-let Result = require("../models/result");
 
+
+
+//home page route
 router.get("/", function (req, res, next) {
   res.render("index", { title: "ResultTracker" });
 });
+
+//contact route
+router.get('/contact', function (req, res, next) {
+  res.render('contact');
+});
+
+//about FAQ route
+router.get("/about", function (req, res, next) {
+  res.render("about");
+});
+
 
 // check results route
 router.get("/checkresults", function (req, res, next) {
   res.render("checkresults", { title: "ResultTracker" });
 });
-
-router.post("/app", function (req, res, next) {
-  let objectData = req.body;
-  delete objectData._csrf;
-  req.session.resultData = objectData;
-  req.session.gp = req.body.gp;
-  res.redirect("save");
-});
-
-router.post("/save", isLoggedIn, function (req, res, next) {
-  let result = new Result({
-    user: req.user,
-    semester: req.body.semester,
-    level: req.body.level,
-    year: req.body.year,
-    gp: req.session.gp,
-    resultsData: req.session.resultData,
-  });
-  result.save(function (err, result) {
-    if (req.session.resultData == null) {
-      req.flash("error", "SORRY NO CALCULATION WAS MADE");
-    } else {
-      req.session.resultData = null;
-      req.session.gp = null;
-      req.flash("success", "Saved Successfully");
-    }
-
-    res.redirect("/dashboard");
-  });
-});
-
-router.get("/show", function (req, res, next) {
-  res.render("user/show");
-});
-
-router.get("/about", function (req, res, next) {
-  res.render("about");
-});
-
 module.exports = router;
 
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  req.session.oldUrl = req.url;
-  res.redirect("/signin");
-}
--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ const multer = require("multer");
 const cloudinary = require("cloudinary");
 const cloudinaryStorage = require("multer-storage-cloudinary");
 
-
+// for cloudinary setup for image upload
 const storage = cloudinaryStorage({
   cloudinary: cloudinary,
   folder: "Profile",
@@ -20,15 +20,55 @@ const parser = multer({ storage: storage });
 let csrfProtection = csrf();
 router.use(csrfProtection);
 
+
+//route for the gp calculte form
 router.get('/app', function (req, res, next) {
   res.render('app', { csrfToken: req.csrfToken() });
 });
 
+
+//route for the result save form
 router.get('/save', isLoggedIn, function (req, res, next) {
   res.render('save', { csrfToken: req.csrfToken() });
 });
 
 
+// post route for saving calculated results to session
+router.post("/app", function (req, res, next) {
+  let objectData = req.body;
+  delete objectData._csrf;
+  req.session.resultData = objectData;
+  req.session.gp = req.body.gp;
+  res.redirect("save");
+});
+
+
+//post route for saving results to database
+router.post("/save", isLoggedIn, function (req, res, next) {
+  let result = new Result({
+    user: req.user,
+    semester: req.body.semester,
+    level: req.body.level,
+    year: req.body.year,
+    gp: req.session.gp,
+    resultsData: req.session.resultData,
+  });
+  result.save(function (err, result) {
+    if (req.session.resultData == null) {
+      req.flash("error", "SORRY NO CALCULATION WAS MADE");
+    } else {
+      req.session.resultData = null;
+      req.session.gp = null;
+      req.flash("success", "Saved Successfully");
+    }
+
+    res.redirect("/dashboard");
+  });
+});
+
+
+
+//route for getting the delete form with user data to be deleted
 router.get('/delete/:_id', isLoggedIn, function (req, res, next) {
   Result.find({ _id: req.params._id },
     function (err, result) {
@@ -39,6 +79,7 @@ router.get('/delete/:_id', isLoggedIn, function (req, res, next) {
     });
 });
 
+//route for deleting user results
 router.get('/delete_confirmed/:_id', isLoggedIn, function (req, res, next) {
   Result.findOneAndRemove({ _id: req.params._id },
     function (err, result) {
@@ -53,7 +94,7 @@ router.get('/delete_confirmed/:_id', isLoggedIn, function (req, res, next) {
 });
 
 
-
+// route for getting the dashboard with all required user data
 router.get('/dashboard', isLoggedIn, function (req, res, next) {
   let successMsg = req.flash('success')[0];
   let messages = req.flash('error');
@@ -86,6 +127,8 @@ router.get('/dashboard', isLoggedIn, function (req, res, next) {
   });
 });
 
+
+//route for getting the edit form with user data to be edited
 router.get('/edit', isLoggedIn, function (req, res, next) {
   let messages = req.flash('error');
 
@@ -94,6 +137,8 @@ router.get('/edit', isLoggedIn, function (req, res, next) {
 
 });
 
+
+//route for editing user data
 router.post('/edit', isLoggedIn, function (req, res, next) {
 
   User.findOne({ email: req.user.email }, function (err, user) {
@@ -141,7 +186,7 @@ router.post('/edit', isLoggedIn, function (req, res, next) {
 
 });
 
-
+//route for uploading profile image 
 router.post('/upload', parser.single("image"), isLoggedIn, function (req, res, next) {
 
   User.findOne({ email: req.user.email }, function (err, user) {
@@ -184,9 +229,7 @@ router.post('/upload', parser.single("image"), isLoggedIn, function (req, res, n
 
 
 
-
-
-
+//route for viewing individual user result
 router.get('/view/:_id', isLoggedIn, function (req, res, next) {
   Result.find({ _id: req.params._id },
     function (err, result) {
@@ -204,25 +247,29 @@ router.get('/view/:_id', isLoggedIn, function (req, res, next) {
     });
 });
 
-
+//route for  loging the user out
 router.get('/logout', isLoggedIn, function (req, res, next) {
   req.logout();
   res.redirect('/');
 });
 
 
+// route for getting the sigin page
 router.get('/signin', isNotLoggedIn, function (req, res, next) {
   let messages = req.flash('error');
   console.log(messages);
   res.render('user/signin', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
 });
 
+// route for signin the user in using passport
 router.post('/signin', isNotLoggedIn, passport.authenticate('local.signin', {
   //successRedirect: '/dashboard',
   failureRedirect: '/signin',
   failureFlash: true
 
-}), function (req, res, next) {
+}), 
+// redirects the user to the last visited page before login.
+function (req, res, next) {
   if (req.session.oldUrl) {
     let oldLink = req.session.oldUrl;
     req.session.oldUrl = null;
@@ -231,19 +278,25 @@ router.post('/signin', isNotLoggedIn, passport.authenticate('local.signin', {
     res.redirect('/dashboard');
   }
 
-});
+      });
 
+
+// route for registering getting the signup page
 router.get('/signup', isNotLoggedIn, function (req, res, next) {
   let messages = req.flash('error');
   res.render('user/signup', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
 });
 
+
+//route for user signup using passport
 router.post('/signup', isNotLoggedIn, passport.authenticate('local.signup', {
   //successRedirect: '/dashboard',
   failureRedirect: '/signup',
   failureFlash: true
 
-}), function (req, res, next) {
+}), 
+// redirects the user to the last visited page before login.
+function (req, res, next) {
   if (req.session.oldUrl) {
     let oldLink = req.session.oldUrl;
     req.session.oldUrl = null;
@@ -255,15 +308,10 @@ router.post('/signup', isNotLoggedIn, passport.authenticate('local.signup', {
 
 });
 
-
-
-router.get('/contact', function (req, res, next) {
-  res.render('contact');
-});
-
-
+// exporting this module
 module.exports = router;
 
+//function to check if the user is logged in
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
@@ -271,7 +319,7 @@ function isLoggedIn(req, res, next) {
   req.session.oldUrl = req.url;
   res.redirect('/signin');
 }
-
+//function to check it the user is not logged in
 function isNotLoggedIn(req, res, next) {
   if (!req.isAuthenticated()) {
     return next();
