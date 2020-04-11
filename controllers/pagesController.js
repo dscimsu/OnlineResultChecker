54e0188890cbecaadd1d797b5975bfde3ff742fc
Victor <okanemechibuikem@gmail.com>
2020-04-11 21:37:03 +0100

    fix alignment bug in dashboard

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -1,20 +1,24 @@
-
 // getting home page
 exports.getHomePage = (req, res, next) => {
-    res.render("index", { title: "ResultTracker" });
-}
+  res.render("index", { title: "ResultTracker" });
+};
 
 // getting contact page
 exports.getContactPage = (req, res, next) => {
-    res.render('contact');
-  }
+  res.render("contact");
+};
 
-  //getting about page
-  exports.getAboutPage = (req, res, next) => {
-    res.render("about");
-  }
+//getting about page
+exports.getAboutPage = (req, res, next) => {
+  res.render("about");
+};
 
-  //getting check result page
-  exports.getCheckResultPage = (req, res, next) => {
-    res.render("checkresults", { title: "ResultTracker" });
-  }
+//getting check result page
+exports.getCheckResultPage = (req, res, next) => {
+  res.render("checkresults", { title: "ResultTracker" });
+};
+
+//getting secondary school dashboard page
+exports.getSecDashboardPage = (req, res, next) => {
+  res.render("secdashboard", { title: "ResultTracker" });
+};
--- routes/pages.js
+++ routes/pages.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const pagesController = require('../controllers/pagesController');
-
+const pagesController = require("../controllers/pagesController");
 
 //home page route
 router.get("/", pagesController.getHomePage);
 
 //contact route
-router.get('/contact', pagesController.getContactPage);
+router.get("/contact", pagesController.getContactPage);
 
 //about FAQ route
 router.get("/about", pagesController.getAboutPage);
 
-
 // check results route
-router.get("/checkresults",pagesController.getCheckResultPage );
+router.get("/checkresults", pagesController.getCheckResultPage);
 
+// Secondary School Dashboard route
+router.get("/secdashboard", pagesController.getSecDashboardPage);
 
 module.exports = router;
-
